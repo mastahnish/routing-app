f4e3fc78e06082327a2aa3cf7c32ec0e788bd33a
Jacek Rys <jacek@myosolutions.pl>
2023-09-05 23:27:25 +0200

    Add no connection view

--- app/src/main/java/pl/jacekrys/routingapp/feature/route/presentation/list/RouteListScreen.kt
+++ app/src/main/java/pl/jacekrys/routingapp/feature/route/presentation/list/RouteListScreen.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +31,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -158,7 +161,68 @@ fun CustomTextField(
                 .padding(8.dp)
         )
     }
+}
 
+@Composable
+fun NoConnectionView(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NoConnectionInfo(modifier = Modifier.weight(1f))
+        Button(
+            onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFD3F268),
+                contentColor = Color.Black
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Retry", style = TextStyle(
+                    fontSize = 24.sp,
+                    lineHeight = 23.5.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF000000),
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
+    }
+}
+
+
+@Composable
+fun NoConnectionInfo(modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_no_connection),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Oops!", style = TextStyle(
+                fontSize = 32.sp,
+                lineHeight = 23.5.sp,
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
+                textAlign = TextAlign.Center,
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "No internet connection", style = TextStyle(
+                fontSize = 20.sp,
+                lineHeight = 23.5.sp,
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
+                textAlign = TextAlign.Center,
+            )
+        )
+    }
 }
 
 @Preview(showSystemUi = true)
@@ -172,4 +236,10 @@ fun RouteListScreenPreview() {
             )
         )
     )
+}
+
+@Preview
+@Composable
+fun NoConnectionPreview() {
+    NoConnectionView(modifier = Modifier)
 }
