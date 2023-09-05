29d855d4598c5d06dd5a6fca277a0523f4581fbf
Jacek Rys <jacek@myosolutions.pl>
2023-09-05 19:34:38 +0200

    Add routes list UI prototype

--- app/src/main/java/pl/jacekrys/routingapp/feature/route/presentation/list/RouteListScreen.kt
+++ app/src/main/java/pl/jacekrys/routingapp/feature/route/presentation/list/RouteListScreen.kt
@@ -1,2 +1,164 @@
 package pl.jacekrys.routingapp.feature.route.presentation.list
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pl.jacekrys.routingapp.R
+import pl.jacekrys.routingapp.feature.route.domain.model.Route
+import pl.jacekrys.routingapp.feature.route.presentation.list.component.RouteItem
+
+@Composable
+fun RouteListScreen(
+    items: List<Route>
+) {
+    Column {
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .clip(RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp))
+                .background(Color.Black)
+                .padding(16.dp)
+        ) {
+            Logo(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                "Good morning!",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                "Where we go today?",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal
+                ),
+            )
+            CustomTextField(
+                value = "Birmingham",
+                onValueChange = {},
+                modifier = Modifier
+                    .padding(top = 4.dp, bottom = 24.dp)
+            )
+        }
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp)
+        ) {
+            item {
+                Spacer(Modifier.height(20.dp))
+            }
+            itemsIndexed(items) { index, item ->
+                RouteItem(modifier = Modifier.fillMaxWidth())
+                if (index < items.lastIndex)
+                    Spacer(Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun Logo(modifier: Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            "Routing App", style = TextStyle(
+                fontSize = 23.sp,
+                fontWeight = FontWeight(700),
+                color = Color(0xFFFFFFFF),
+            )
+        )
+    }
+}
+
+@Preview(backgroundColor = 0xFF000000)
+@Composable
+fun LogoPreview() {
+    Logo(modifier = Modifier)
+}
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 35.dp)
+            .clip(RoundedCornerShape(100))
+            .background(Color.White)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Search, contentDescription = null,
+            modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+        )
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp)
+        )
+    }
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun RouteListScreenPreview() {
+    RouteListScreen(
+        items = listOf(
+            Route("", "", listOf()),
+            Route("", "", listOf())
+        )
+    )
+}
---
+++ app/src/main/java/pl/jacekrys/routingapp/feature/route/presentation/list/component/RouteItem.kt
@@ -0,0 +1,76 @@
+package pl.jacekrys.routingapp.feature.route.presentation.list.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.sp as sp1
+
+@Composable
+fun RouteItem(modifier: Modifier) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFFBFEF3))
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                "Batchelder bus 54", style = TextStyle(
+                    fontSize = 20.sp,
+                    lineHeight = 16.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF000000),
+                )
+            )
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFD3F268))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowForward,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RouteItemPreview() {
+    RouteItem(Modifier)
+}
