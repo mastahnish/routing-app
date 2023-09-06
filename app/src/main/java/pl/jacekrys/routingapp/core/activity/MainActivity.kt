b94c6eff519f9edd4dc6ab3b6f2ca871d0a01311
Jacek Rys <jacek@myosolutions.pl>
2023-09-06 13:04:36 +0200

    Remove unused code

--- app/src/main/java/pl/jacekrys/routingapp/core/activity/MainActivity.kt
+++ app/src/main/java/pl/jacekrys/routingapp/core/activity/MainActivity.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.findNavController
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
@@ -24,7 +18,6 @@ import org.koin.compose.koinInject
 import pl.jacekrys.routingapp.core.navigation.NavigationEvent
 import pl.jacekrys.routingapp.core.navigation.Navigator
 import pl.jacekrys.routingapp.core.navigation.Screen
-import pl.jacekrys.routingapp.core.navigation.getLastBackStackDestinationRoute
 import pl.jacekrys.routingapp.core.ui.theme.RoutingAppTheme
 import pl.jacekrys.routingapp.feature.route.presentation.details.RouteDetailsScreen
 import pl.jacekrys.routingapp.feature.route.presentation.details.RouteDetailsViewModel
@@ -79,21 +72,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoutingAppTheme {
-        Greeting("Android")
     }
 }
